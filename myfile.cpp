fddb65ad5617c2ea3ca4f14cd25cec14f8d6d3f6
Basmala Khalifa <elseedybasmala@gmail.com>
2026-04-23 22:26:46 +0200

    First Structure

---
+++ myfile.cpp
@@ -0,0 +1,14 @@
+// Names:                IDs:
+// ----------------------------
+// Basmala Khalifa       20226122
+// Amira Emad            20226017
+// Raghad Ahmed          20226041
+
+// Extended BNF (EBNF) Grammar:
+// ----------------------------
+// expr   -> factor { '.' factor }      left associative (product)
+// factor -> base [ '^' '-' '1' ]       inverse (right binding, optional)
+// base   -> '(' expr ')' | id
+// id     -> any single lowercase letter (x, y, z, e, a, b, ...)
+
+#include "myfile.h"
