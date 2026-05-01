41b2598cb67699a0b87aba92880431e9f8d5eb83
Raghad-r4 <rghdahmd2004@gmail.com>
2026-05-01 10:44:15 +0300

    Finalize parser implementation

--- myfile.cpp
+++ myfile.cpp
@@ -60,7 +60,7 @@ struct CompilerInfo
     OutFile out_file;
 
     CompilerInfo(const char* in_str, const char* out_str)
-                : in_file(in_str), out_file(out_str)
+        : in_file(in_str), out_file(out_str)
     {
     }
 };
@@ -70,11 +70,6 @@ struct CompilerInfo
 enum TokenType
 {
     ID, PRODUCT, INVERSE, LEFT_PAREN, RIGHT_PAREN, ENDFILE, ERROR
-};
-
-const char* TokenTypeStr[]=
-{
-    "ID", "PRODUCT", "INVERSE", "LEFT_PAREN", "RIGHT_PAREN", "ENDFILE", "ERROR"
 };
 
 struct Token
@@ -92,6 +87,7 @@ const Token symbolic_tokens[]=
     Token(LEFT_PAREN, '('),
     Token(RIGHT_PAREN, ')')
 };
+
 const int num_symbolic_tokens=sizeof(symbolic_tokens)/sizeof(symbolic_tokens[0]);
 
 void GetNextToken(CompilerInfo* pci, Token* ptoken)
@@ -118,26 +114,31 @@ void GetNextToken(CompilerInfo* pci, Token* ptoken)
     {
         char c2=pci->in_file.GetNextChar();
         char c3=pci->in_file.GetNextChar();
-        if(c2=='-' && c3=='1') {ptoken->type=INVERSE; ptoken->ch=0; return;}
-        ptoken->type=ERROR; ptoken->ch=ch; return;
+        if(c2=='-' && c3=='1')
+        {
+            ptoken->type=INVERSE;
+            return;
+        }
+        ptoken->type=ERROR;
+        return;
     }
 
-    if(ch>='a' && ch<='z') {ptoken->type=ID; ptoken->ch=ch; return;}
+    if(ch>='a' && ch<='z')
+    {
+        ptoken->type=ID;
+        ptoken->ch=ch;
+        return;
+    }
 
-    ptoken->type=ERROR; ptoken->ch=ch;
+    ptoken->type=ERROR;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
-//Parser
+// Parser
 
 enum NodeKind
 {
     PRODUCT_NODE, INVERSE_NODE, ID_NODE
-};
-
-const char* NodeKindStr[]=
-{
-    "product", "inverse", "ID"
 };
 
 #define MAX_CHILDREN 2
@@ -147,11 +148,12 @@ struct TreeNode
     TreeNode* child[MAX_CHILDREN];
     NodeKind node_kind;
     char id;
+
     TreeNode()
     {
         int i;
-        for(i = 0;i < MAX_CHILDREN;i++) child[i] = 0;
-        id = 0;
+        for(i=0;i<MAX_CHILDREN;i++) child[i]=0;
+        id=0;
     }
 };
 
@@ -162,83 +164,143 @@ struct ParseInfo
 
 void Match(CompilerInfo* pci, ParseInfo* ppi, TokenType expected_type)
 {
-    if (ppi->next_token.type != expected_type) throw 0;
+    if(ppi->next_token.type!=expected_type) throw 0;
     GetNextToken(pci, &ppi->next_token);
 }
 
 TreeNode* Expr(CompilerInfo*, ParseInfo*);
 
-//base -> '(' expr ')' | id
 TreeNode* Base(CompilerInfo* pci, ParseInfo* ppi)
 {
-    TreeNode* tree = 0;
-    if (ppi->next_token.type == ID)
+    TreeNode* tree=0;
+
+    if(ppi->next_token.type==ID)
     {
-        tree = new TreeNode();
-        tree->node_kind = ID_NODE;
-        tree->id = ppi->next_token.ch;
+        tree=new TreeNode();
+        tree->node_kind=ID_NODE;
+        tree->id=ppi->next_token.ch;
         Match(pci, ppi, ID);
         return tree;
     }
-    if (ppi->next_token.type == LEFT_PAREN)
+
+    if(ppi->next_token.type==LEFT_PAREN)
     {
         Match(pci, ppi, LEFT_PAREN);
-        tree = Expr(pci, ppi);
+        tree=Expr(pci, ppi);
         Match(pci, ppi, RIGHT_PAREN);
         return tree;
     }
+
     throw 0;
-    return 0;
 }
 
-//factor -> base [ '^-1' ]  inverse is right binding and optional
 TreeNode* Factor(CompilerInfo* pci, ParseInfo* ppi)
 {
-    TreeNode* tree = Base(pci, ppi);
-    if (ppi->next_token.type == INVERSE)
+    TreeNode* tree=Base(pci, ppi);
+
+    if(ppi->next_token.type==INVERSE)
     {
-        TreeNode* inv_tree = new TreeNode;
-        inv_tree->node_kind = INVERSE_NODE;
-        inv_tree->child[0] = tree;
+        TreeNode* inv_tree=new TreeNode();
+        inv_tree->node_kind=INVERSE_NODE;
+        inv_tree->child[0]=tree;
         Match(pci, ppi, INVERSE);
         return inv_tree;
     }
+
     return tree;
 }
 
-//expr -> factor { '.' factor } left associative
 TreeNode* Expr(CompilerInfo* pci, ParseInfo* ppi)
 {
-    TreeNode* tree = Factor(pci, ppi);
-    while (ppi->next_token.type == PRODUCT)
+    TreeNode* tree=Factor(pci, ppi);
+
+    while(ppi->next_token.type==PRODUCT)
     {
-        TreeNode* prod_tree = new TreeNode();
-        prod_tree->node_kind = PRODUCT_NODE;
-        prod_tree->child[0] = tree;
+        TreeNode* prod_tree=new TreeNode();
+        prod_tree->node_kind=PRODUCT_NODE;
+        prod_tree->child[0]=tree;
+
         Match(pci, ppi, PRODUCT);
-        prod_tree->child[1] = Factor(pci, ppi);
-        tree = prod_tree;
+
+        prod_tree->child[1]=Factor(pci, ppi);
+        tree=prod_tree;
     }
+
     return tree;
 }
 
 TreeNode* Parse(CompilerInfo* pci)
 {
     ParseInfo parse_info;
+
     GetNextToken(pci, &parse_info.next_token);
-    TreeNode* syntax_tree = Expr(pci, &parse_info);
-    if (parse_info.next_token.type != ENDFILE)
-        printf("Error: expression ends before file ends :(");
-    return syntax_tree;
+
+    TreeNode* tree=Expr(pci, &parse_info);
+
+    if(parse_info.next_token.type!=ENDFILE)
+        printf("Error\n");
+
+    return tree;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
 // Print
 
+void PrintTree(TreeNode* tree, int level)
+{
+    if(!tree) return;
+
+    int i;
+    for(i=0;i<level;i++) printf("   ");
+
+    if(level>0) printf("|--");
+
+    if(tree->node_kind==PRODUCT_NODE)
+        printf("product\n");
+    else if(tree->node_kind==INVERSE_NODE)
+        printf("inverse\n");
+    else
+        printf("%c\n", tree->id);
+
+    for(i=0;i<MAX_CHILDREN;i++)
+        PrintTree(tree->child[i], level+1);
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+// Destroy
+
+void DestroyTree(TreeNode* tree)
+{
+    if(!tree) return;
+
+    int i;
+    for(i=0;i<MAX_CHILDREN;i++)
+        DestroyTree(tree->child[i]);
+
+    delete tree;
+}
+
 ////////////////////////////////////////////////////////////////////////////////////
 // Main
+
 int main()
 {
+    CompilerInfo compiler("input.txt", "output.txt");
+
+    TreeNode* tree=0;
+
+    try
+    {
+        tree=Parse(&compiler);
+        PrintTree(tree, 0);
+    }
+    catch(...)
+    {
+        printf("Parse Error\n");
+    }
+
+    DestroyTree(tree);
+
     return 0;
 }
 
